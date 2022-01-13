4a4c5878e94bc8284fc2823de1dee9b39e958212
Ricardo Cardoso <silvasistemas@gmail.com>
2022-01-12 23:00:58 -0300

    Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -4,18 +4,18 @@ import './styles.css';
 function Navbar(){
 
     return (
-    <header>
+        <header>
         <nav className="container">
-          <div className="dsmovie-nav-content">
-             <h1>DSMovie</h1>
-                <a href="https://github.com/devsuperior">
-                   <div className="dsmovie-contact-container">
-                    <GithubIcon />
-                    <p>/devsuperior</p>
-                  </div>
-                 </a>
-             </div>
-         </nav>
+            <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/devsuperior</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
     </header>
     )
     
